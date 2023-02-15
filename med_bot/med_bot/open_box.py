9b70f69ac490a1db9b8d5a34ca81480ce359aef4
RapinAd <rapin.adc@gmail.com>
2023-02-15 17:34:45 +0000

    --

--- med_bot/med_bot/open_box.py
+++ med_bot/med_bot/open_box.py
@@ -15,10 +15,10 @@ class OpenBox(Node):  # MODIFY NAME
             Joy, "joy", self.open_box_callback, 10)
         self.get_logger().info("--Open Box Node has been started--")
         #PIN NUMBER
-        self.servo1_pin = 25
-        self.servo2_pin = 8
-        self.servo3_pin = 7
-        self.servo4_pin = 1
+        self.servo1_pin = 12
+        self.servo2_pin = 16
+        self.servo3_pin = 20
+        self.servo4_pin = 21
 
         #GPIO SETUP
         GPIO.setmode(GPIO.BCM)
@@ -38,65 +38,88 @@ class OpenBox(Node):  # MODIFY NAME
         self.servo3.start(12)
         self.servo4.start(12)
 
+        #BOX_STATUS
+        self.box1_status = 0
+        self.box2_status = 0
+        self.box3_status = 0
+        self.box4_status = 0
+
     def open(self, servo):
         print("Box is opening")
         duty = 12
+        servo.start(duty)
         while duty >= 8.65:
             servo.ChangeDutyCycle(duty)
             duty = duty - 0.1
-            time.sleep(0.1)
+            time.sleep(0.02)
+            servo.ChangeDutyCycle(0)
+            time.sleep(0.03)
 
     def close(self, servo):
         print("Box is closing")
         duty = 8.65
+        servo.start(duty)
         while duty <= 12:
             servo.ChangeDutyCycle(duty)
-            duty = duty + 0.1
-            time.sleep(0.1)
+            duty = duty + 0.12
+            time.sleep(0.02)
+            servo.ChangeDutyCycle(0)
+            time.sleep(0.03)
 
     def open_box_callback(self, msg):
         button1_status = msg.buttons[0]
         button2_status = msg.buttons[1]
         button3_status = msg.buttons[2]
         button4_status = msg.buttons[3]
-        #status 0 --> close, 1 --> open
-        box1_status = 0
-        box2_status = 0
-        box3_status = 0
-        box4_status = 0
 
-        print("Button 1 Status: ", button1_status, "    |    Box 1 Status: ", box1_status)
-        print("Button 2 Status: ", button2_status, "    |    Box 1 Status: ", box2_status)
-        print("Button 3 Status: ", button3_status, "    |    Box 1 Status: ", box3_status)
-        print("Button 4 Status: ", button4_status, "    |    Box 1 Status: ", box4_status)
+        print("Button 1 Status: ", button1_status, "    |    Box 1 Status: ", self.box1_status)
+        print("Button 2 Status: ", button2_status, "    |    Box 1 Status: ", self.box2_status)
+        print("Button 3 Status: ", button3_status, "    |    Box 1 Status: ", self.box3_status)
+        print("Button 4 Status: ", button4_status, "    |    Box 1 Status: ", self.box4_status)
 
-        if button1_status == 1 and box1_status == 0:
-            self.open(self.servo1)
-            box1_status = 1
-        elif button1_status == 1 and box1_status == 1:
+        if button1_status == 1 and self.box1_status == 0:
+            self.open(self.servo1) #status 0 --> close, 1 --> open
+            self.box1_status = 1
+        elif button1_status == 1 and self.box1_status == 1:
             self.close(self.servo1)
-            box1_status = 0
+            self.box1_status = 0
+        elif button1_status == 0 and self.box1_status == 0:
+            self.servo1.ChangeDutyCycle(0)
+        else:
+            self.servo1.ChangeDutyCycle(0)
 
-        if button2_status == 1 and box2_status == 0:
+        if button2_status == 1 and self.box2_status == 0:
             self.open(self.servo2)
-            box2_status = 1
-        elif button2_status == 1 and box2_status == 1:
+            self.box2_status = 1
+        elif button2_status == 1 and self.box2_status == 1:
             self.close(self.servo2)
-            box2_status = 0
+            self.box2_status = 0
+        elif button2_status == 0 and self.box2_status == 0:
+            self.servo2.ChangeDutyCycle(0)
+        else:
+            self.servo2.ChangeDutyCycle(0)
 
-        if button3_status == 1 and box3_status == 0:
+        if button3_status == 1 and self.box3_status == 0:
             self.open(self.servo3)
-            box3_status = 1
-        elif button3_status == 1 and box3_status == 1:
+            self.box3_status = 1
+        elif button3_status == 1 and self.box3_status == 1:
             self.close(self.servo3)
-            box3_status = 0
+            self.box3_status = 0
+        elif button3_status == 0 and self.box3_status == 0:
+            self.servo3.ChangeDutyCycle(0)
+        else:
+            self.servo3.ChangeDutyCycle(0)
 
-        if button4_status == 1 and box4_status == 0:
+        if button4_status == 1 and self.box4_status == 0:
             self.open(self.servo4)
-            box4_status = 1
-        elif button4_status == 1 and box4_status == 1:
+            self.box4_status = 1
+        elif button4_status == 1 and self.box4_status == 1:
             self.close(self.servo4)
-            box4_status = 0
+            self.box4_status = 0
+        elif button4_status == 0 and self.box4_status == 0:
+            self.servo4.ChangeDutyCycle(0)
+        else:
+            self.servo4.ChangeDutyCycle(0)
 
 def main(args=None):
     rclpy.init(args=args)
--- med_bot/scripts/servo_control.py
+++ med_bot/scripts/servo_control.py
@@ -1,29 +1,31 @@
 import RPi.GPIO as GPIO
 import time
 
+
 servo = 12
 
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(servo, GPIO.OUT)
 
-servo1 = GPIO.PWM(servo , 50)
+servo1 = GPIO.PWM(servo , 50 , initial=False)
 
 servo1.start(0)
 
 duty = 12
 
+
 while duty >= 8.65:
     servo1.ChangeDutyCycle(duty)
-    duty = duty - 0.1
-    time.sleep(0.1)
+    duty = duty - 0.2
+    time.sleep(0.05)
 
 time.sleep(2)
 
 while duty <= 12:
     servo1.ChangeDutyCycle(duty)
-    duty = duty + 0.1
-    time.sleep(0.1)
+    duty = duty + 0.2
+    time.sleep(0.05)
 
 
 servo1.stop()
